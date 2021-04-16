885a13f5a6f0a8ba4bef0f649630c81071e4b467
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-04-16 08:43:47 +0700

    implement button on form

--- src/components/Form/Form.svelte
+++ src/components/Form/Form.svelte
@@ -1,10 +1,14 @@
 <script>
     import Input from './Input.svelte'
+    import Button from './Button.svelte'
 </script>
 
 <form>
     <Input placeholder="Name" />
     <Input placeholder="Phone Number" />
+    <div class="form__buttonWrapper">
+        <Button>Add</Button>
+    </div>
 </form>
 
 <style>
@@ -15,5 +19,9 @@
         margin: 0 auto;
 
         display: grid;
+    }
+
+    .form__buttonWrapper {
+        text-align: center;
     }
 </style>
