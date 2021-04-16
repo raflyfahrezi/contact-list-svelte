73ebaff2275795b8d8edc451727061c9043c9168
Farhan Rafly Fahrezi Saepulloh <raflyfahr15@gmail.com>
2021-04-16 09:57:15 +0700

    implement list

--- src/components/List/List.svelte
+++ src/components/List/List.svelte
@@ -5,11 +5,15 @@
 </script>
 
 <div>
-    {#each contactList as { name, number }, i}
-        <div class="card">
-            <p>{i + 1}. {name} &middot; {number}</p>
-        </div>
-    {/each}
+    {#if contactList.length > 0}
+        {#each contactList as { name, number }, i}
+            <div class="card">
+                <p>{i + 1}. {name} &middot; {number}</p>
+            </div>
+        {/each}
+    {:else}
+        <p>No Data</p>
+    {/if}
 </div>
 
 <style>
